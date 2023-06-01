b6f2c6a951f92fc2ae4fc364b661ed5008c5cfe1
JEFFERSON_RESURRECCION <resujeff27@gmail.com>
2023-06-01 13:46:22 +0800

    Date Selector

---
+++ src/components/DateSelector.jsx
@@ -0,0 +1,17 @@
+import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+
+export default function DateSelector() {
+  return (
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <DemoContainer components={["DatePicker"]}>
+        <DatePicker
+          label="Select Date"
+          className="bg-[#E8E8E8] bg-opacity-50 hover:bg-opacity-80 duration-300 w-[400px]"
+        />
+      </DemoContainer>
+    </LocalizationProvider>
+  );
+}
