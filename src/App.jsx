cd68ce72b1983ce05e2d7620cd576e0c2ea75ecc
JEFFERSON_RESURRECCION <resujeff27@gmail.com>
2023-06-16 13:47:35 +0800

    Add Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,30 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Footer from "./components/Footer";
 import Navigation from "./components/Navigation";
 import "./index.css";
 import "./navigationToogle.css";
 import Home from "./pages/Home";
+import AboutUs from "./pages/AboutUs";
 
 function App() {
   return (
     <div className="w-full">
-      <Navigation />
-      <div className="px-2 w-full">
-        <Home />
-      </div>
-      <Footer />
+      <BrowserRouter>
+        <Navigation />
+
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/about-us" element={<AboutUs />} />
+        </Routes>
+
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
 
 export default App;
+
+{
+  /* <Home /> */
+}
