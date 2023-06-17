3a285a0050d5d4c6e3d15ec15685a094e15993c9
JEFFERSON_RESURRECCION <resujeff27@gmail.com>
2023-06-17 10:49:28 +0800

    Add History and Service in About Page

---
+++ src/components/HistoryAbout.jsx
@@ -0,0 +1,33 @@
+import { Button } from "@mui/material";
+import { Qoute, LastQoute } from "./Icons";
+
+export default function HistoryAbout() {
+  return (
+    <div className="bg-[url(./assets/webpic/about.jpg)] bg-cover bg-center h-screen ">
+      <div className="flex item-center bg-gradient-to-r from-white via-transparent h-screen absolute">
+        <div className=" flex flex-col item-center justify-center lg:max-w-[700px] lg:ml-64 text-center text-[#292A2E]">
+          <p className="font-head text-4xl text-bold">Resto Bar History</p>
+          <p className="font-head text-sm my-5 justify-center flex">
+            <Qoute />
+            Welcome to Foodism Resto Bar, where history meets culinary
+            excellence!
+            <LastQoute />
+          </p>
+          <p className="font-body text-base max-w-[550px] mx-auto text-[#292A2E] hover:font-semibold duration-300 ease-in-out">
+            Established in [year], our resto bar has been a prominent fixture in
+            the vibrant dining scene. With [number] years of experience, we have
+            continuously evolved, enchanting guests with our remarkable heritage
+            and unwavering commitment to culinary excellence.
+            <br />
+            <br />
+            Through the years, our resto bar has become a gathering place for
+            food enthusiasts, connoisseurs, and those seeking an extraordinary
+            dining adventure. We take pride in sourcing only the finest
+            ingredients, supporting local producers and farmers, and ensuring
+            that each dish we serve tells a story of quality and flavor.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/ServiceAbout.jsx
@@ -0,0 +1,20 @@
+import { AboutData } from "../data/data";
+
+export default function ServiceAbout() {
+  return (
+    <div className="w-full bg-white py-20 flex items-center justify-center gap-20 flex-wrap">
+      {AboutData.map((item) => (
+        <div
+          className="serviceCard max-w-[350px] text-center p-10 flex flex-col items-center justify-center gap-3"
+          key={item.id}
+        >
+          <img src={item.image} alt="Service Icon" className="w-24" />
+          <p className="font-head font-bold text-xl serviceTitle">
+            {item.title}
+          </p>
+          <p className="font-body serviceContent">{item.desc}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
